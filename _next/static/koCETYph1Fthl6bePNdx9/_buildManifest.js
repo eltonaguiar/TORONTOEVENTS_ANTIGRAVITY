8349cc5f0646f277ead21ecb8fbb3d19bcaab0eb
MiracleBoot Developer <you@example.com>
2026-01-25 16:25:33 -0500

    Updates

--- _next/static/koCETYph1Fthl6bePNdx9/_buildManifest.js
+++ _next/static/koCETYph1Fthl6bePNdx9/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/TORONTOEVENTS_ANTIGRAVITY//_next/:path+",
-        "destination": "/TORONTOEVENTS_ANTIGRAVITY/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
